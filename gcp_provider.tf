57dc1707417cf0ee48613972f2da5095509ee008
Hari Sekhon <harisekhon@gmail.com>
2020-02-03 13:38:21 +0000

    added gcp_provider.tf

---
+++ gcp_provider.tf
@@ -0,0 +1,15 @@
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-03 13:37:32 +0000 (Mon, 03 Feb 2020)
+#
+#  https://github.com/harisekhon/devops-perl-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+provider "google" {}
