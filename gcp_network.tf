7781556bcb32d4c830c0148a3ea12cecc0b5abd7
Hari Sekhon <harisekhon@gmail.com>
2020-02-03 14:45:12 +0000

    added gcp_network.tf

---
+++ gcp_network.tf
@@ -0,0 +1,48 @@
+#
+#  Author: Hari Sekhon
+#  Date: 2020-02-03 13:37:32 +0000 (Mon, 03 Feb 2020)
+#
+#  https://github.com/harisekhon/devops-perl-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+  name                    = "mynetwork"
+  auto_create_subnetworks = "true"
+}
+
+# Add a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+  name    = "mynetwork-allow-http-ssh-rdp-icmp"
+  network = google_compute_network.mynetwork.self_link
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+}
+
+# mynet-us-vm instance
+module "mynet-us-vm" {
+  source              = "./instance"
+  instance_name       = "mynet-us-vm"
+  instance_zone       = "us-central1-a"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
+}
+
+# mynet-eu-vm instance
+module "mynet-eu-vm" {
+  source              = "./instance"
+  instance_name       = "mynet-eu-vm"
+  instance_zone       = "europe-west1-d"
+  instance_subnetwork = google_compute_network.mynetwork.self_link
+}
