766ce84d35a24c9ad81e742c386592973d425884
Hari Sekhon <harisekhon@gmail.com>
2020-03-13 18:57:13 +0000

    added template.zsh

---
+++ template.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+#
+#  Author: Hari Sekhon
+#  Date: 2020-03-13 18:56:51 +0000 (Fri, 13 Mar 2020)
+#
+#  https://github.com/harisekhon/devops-perl-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
