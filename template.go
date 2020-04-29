aaaaf2bfc51bed5f4369155b99d808b4cd7df39f
Hari Sekhon <harisekhon@gmail.com>
2020-04-29 15:16:09 +0100

    imported template.go

---
+++ template.go
@@ -0,0 +1,25 @@
+//  vim:ts=4:sts=4:sw=4:et
+//
+//  Author: Hari Sekhon
+//  Date: [% DATE %]
+//
+//  [% URL %]
+//
+//  [% LICENSE %]
+//
+//  [% MESSAGE %]
+//
+//  [% LINKEDIN %]
+//
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main(){
+    
+}
