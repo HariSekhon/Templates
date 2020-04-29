39e5add86a05524328b25d60030d99fed9c65019
Hari Sekhon <harisekhon@gmail.com>
2020-04-29 15:21:26 +0100

    imported template.rb

---
+++ template.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#  [% VIM_TAGS %]
+#
+#  Author: Hari Sekhon
+#  Date: [% DATE # 2010-06-17 14:56:13 +0100 (Thu, 17 Jun 2010) %]
+#
+#  [% URL %]
+#
+#  [% LICENSE %]
+#
+#  [% MESSAGE %]
+#
+#  [% LINKEDIN %]
+#
+
+
