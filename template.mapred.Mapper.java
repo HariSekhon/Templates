6986918b0e7e3d8a311b74c40a2fb585e3806a8d
Hari Sekhon <harisekhon@gmail.com>
2020-04-29 15:18:16 +0100

    imported template.mapred.Mapper.java

---
+++ template.mapred.Mapper.java
@@ -0,0 +1,29 @@
+//
+//  Author: Hari Sekhon
+//  Date: [% DATE # 2013-04-18 14:38:57 +0100 (Thu, 18 Apr 2013) %]
+//
+//  [% URL %]
+//
+//  [% LICENSE %]
+//
+//  [% VIM_TAGS %]
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+// input, input, output, output
+public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        // TODO: put logic here
+
+        context.write(key, new IntWritable(value));
+    
+    }
+}
