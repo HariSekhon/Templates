c7e829482008165fe5505a3f28ec3645c28c479a
Hari Sekhon <harisekhon@gmail.com>
2020-06-01 20:02:58 +0100

    updated template.php

--- template.php
+++ template.php
@@ -1,9 +1,11 @@
 <?php
-#
-#  Author: Hari Sekhon
-#  Date: [% DATE # 2009-04-10 16:12:53 +0100 (Fri, 10 Apr 2009) %]
-#
-#  [% VIM_TAGS %]
+//
+//  Author: Hari Sekhon
+//  Date: [% DATE # 2009-04-10 16:12:53 +0100 (Fri, 10 Apr 2009) %]
+//
+//  [% VIM_TAGS %]
+//
+
 
 
 ?>
