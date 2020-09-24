09121563ed2e4e959661f2e5d0b521c0fdcd814a
Hari Sekhon <harisekhon@gmail.com>
2020-09-24 19:04:27 +0100

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -16,47 +16,47 @@ terraform {
   # XXX: remember to enable Object Versioning on this GCS bucket for state recovery:
   #
   #		https://cloud.google.com/storage/docs/object-versioning
-	#
+  #
   backend "gcs" {
-    bucket = "NAME-prod-tf-state"  # XXX: EDIT
-    prefix  = "terraform/state/prod"  # <prefix>/<name>.tfstate
+    bucket = "NAME-prod-tf-state"   # XXX: EDIT
+    prefix = "terraform/state/prod" # <prefix>/<name>.tfstate
   }
 
   # XXX: remember to enable Bucket Versioning on this S3 bucket for state recovery:
-	#
-	#		https://docs.aws.amazon.com/AmazonS3/latest/user-guide/enable-versioning.html
-	#
+  #
+  #		https://docs.aws.amazon.com/AmazonS3/latest/user-guide/enable-versioning.html
+  #
   # IAM permissions needed:
-	#
-	#		https://www.terraform.io/docs/backends/types/s3.html#s3-bucket-permissions
-	#
-#  backend "s3" {
-#    bucket = "mybucket" # XXX: EDIT
-#    key    = "path/to/my/key"
-#    region = "eu-west-1"
-#    # XXX: EDIT - set DynamoDB table name and configure for locking:
-#    #
-#    #  https://www.terraform.io/docs/backends/types/s3.html#dynamodb-state-locking
-#    #
-#    dynamodb_table = "mytable"
-#  }
+  #
+  #		https://www.terraform.io/docs/backends/types/s3.html#s3-bucket-permissions
+  #
+  #  backend "s3" {
+  #    bucket = "mybucket" # XXX: EDIT
+  #    key    = "path/to/my/key"
+  #    region = "eu-west-1"
+  #    # XXX: EDIT - set DynamoDB table name and configure for locking:
+  #    #
+  #    #  https://www.terraform.io/docs/backends/types/s3.html#dynamodb-state-locking
+  #    #
+  #    dynamodb_table = "mytable"
+  #  }
 
   # default backend
-#  backend "local" {
-#    path = "./terraform.tfstate"
-#  }
+  #  backend "local" {
+  #    path = "./terraform.tfstate"
+  #  }
 
   # Terraform Cloud
-#  backend "remote" {
-#    hostname = "app.terraform.io"
-#    organization = "mycompany"  # XXX: EDIT
-#
-#    workspaces {
-#      # XXX: EDIT
-#      # single workspace
-#      name = "my-app-prod"
-#      # or multiple workspaces
-#      prefix = "my-app-"
-#    }
-#  }
+  #  backend "remote" {
+  #    hostname = "app.terraform.io"
+  #    organization = "mycompany"  # XXX: EDIT
+  #
+  #    workspaces {
+  #      # XXX: EDIT
+  #      # single workspace
+  #      name = "my-app-prod"
+  #      # or multiple workspaces
+  #      prefix = "my-app-"
+  #    }
+  #  }
 }
