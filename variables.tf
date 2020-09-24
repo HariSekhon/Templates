a4be4db4a4fdb6ed8d89d1396a90cacd355a7a09
Hari Sekhon <harisekhon@gmail.com>
2020-09-24 17:20:46 +0100

    added variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+#  [% VIM_TAGS %]
+#
+#  Author: Hari Sekhon
+#  Date: [% DATE # 2020-08-21 10:14:10 +0100 (Fri, 21 Aug 2020) %]
+#
+#  [% URL %]
+#
+#  [% LICENSE %]
+#
+#  [% MESSAGE %]
+#
+#  [% LINKEDIN %]
+#
+
+variable "project" {
+  default = "myproject-123456"  # XXX: EDIT
+}
+variable "vpc_name" {
+  default = "default"  # XXX: EDIT
+}
+variable "region" {
+  default = "europe-west1"  # XXX: EDIT
+}
