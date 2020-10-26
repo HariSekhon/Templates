6ec4e3341d7a7a56e0d1c79860407a1dd7755b99
Hari Sekhon <harisekhon@gmail.com>
2020-10-26 19:49:44 +0000

    added template.tf

---
+++ template.tf
@@ -0,0 +1,15 @@
+#  vim:ts=2:sts=2:sw=2:et
+#
+#  Author: Hari Sekhon
+#  Date: [% DATE # 2020-08-21 10:14:10 +0100 (Fri, 21 Aug 2020) %]
+#
+#  [% URL %]
+#
+#  [% LICENSE %]
+#
+#  [% MESSAGE %]
+#
+#  [% LINKEDIN %]
+#
+
+
