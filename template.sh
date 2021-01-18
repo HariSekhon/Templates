b8846193544dc043258faf91846eaa7f69db1a76
Hari Sekhon <harisekhon@gmail.com>
2021-01-18 11:26:24 +0000

    updated template.sh

--- template.sh
+++ template.sh
@@ -15,7 +15,7 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+srcdir="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck disable=SC1090
 . "$srcdir/lib/utils.sh"
