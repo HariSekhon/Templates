008a15ec62a8d628d6e0bacffb846935af76ce37
Hari Sekhon <harisekhon@gmail.com>
2021-10-08 17:06:01 +0100

    updated cloudbuild.groovy

--- vars/cloudbuild.groovy
+++ vars/cloudbuild.groovy
@@ -2,7 +2,7 @@
 //  Author: Hari Sekhon
 //  Date: 2021-09-01 12:50:03 +0100 (Wed, 01 Sep 2021)
 //
-//  vim:ts=4:sts=4:sw=4:noet
+//  vim:ts=4:sts=4:sw=4:et
 //
 //  https://github.com/HariSekhon/templates
 //
