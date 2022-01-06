56bf79bf18ece84c66f25a5d38d0c5b64c4ce1e2
Hari Sekhon <harisekhon@gmail.com>
2022-01-06 17:50:59 +0000

    updated terraformInit.groovy

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -28,13 +28,17 @@ def call(timeoutMinutes=10){
         //
         // terraform docker image doesn't have bash
         //sh '''#/usr/bin/env bash -euxo pipefail
-        sh '''#/bin/sh -eux
+        sh
+          label: 'Workspace Select'
+          script: '''#/bin/sh -eux
           if [ -n "$TF_WORKSPACE" ]; then
               terraform workspace new "$TF_WORKSPACE" || echo "Workspace '$TF_WORKSPACE' already exists or using Terraform Cloud as a backend"
               #terraform workspace select "$TF_WORKSPACE"  # TF_WORKSPACE takes precedence over this select
           fi
-          terraform init -input=false  # -backend-config "bucket=$ACCOUNT-$PROJECT-terraform" -backend-config "key=${ENV}-${PRODUCT}/${COMPONENT}/state.tf"
-        '''
+          '''
+        sh
+          label: 'Terraform Init'
+          script: 'terraform init -input=false  # -backend-config "bucket=$ACCOUNT-$PROJECT-terraform" -backend-config "key=${ENV}-${PRODUCT}/${COMPONENT}/state.tf" '
       }
     }
   }
