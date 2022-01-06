e82b47be5a4db5ccb7a50993c7bfd59d1e0e80a6
Hari Sekhon <harisekhon@gmail.com>
2022-01-06 18:14:30 +0000

    updated terragruntPlan.groovy

--- vars/terragruntPlan.groovy
+++ vars/terragruntPlan.groovy
@@ -20,17 +20,15 @@ def call(timeoutMinutes=10){
 
   // XXX: set Terragrunt version in the docker image tag in jenkins-agent-pod.yaml
   container('terragrunt') {
-    steps {
-      //dir ("components/${COMPONENT}") {
-      ansiColor('xterm') {
-        // alpine/terragrunt docker image doesn't have bash
-        //sh '''#/usr/bin/env bash -euxo pipefail
-        //sh '''#/bin/sh -eux
-        sh label: 'Workspace List',
-           script: 'terragrunt workspace list || :'  // # 'workspaces not supported' if using Terraform Cloud as a backend
-        sh label: 'Terragrunt Plan',
-           script: 'terragrunt plan --terragrunt-non-interactive -out=plan.zip -input=false'  // # -var-file=base.tfvars -var-file="$ENV.tfvars"
-      }
+    //dir ("components/${COMPONENT}") {
+    ansiColor('xterm') {
+      // alpine/terragrunt docker image doesn't have bash
+      //sh '''#/usr/bin/env bash -euxo pipefail
+      //sh '''#/bin/sh -eux
+      sh label: 'Workspace List',
+         script: 'terragrunt workspace list || :'  // # 'workspaces not supported' if using Terraform Cloud as a backend
+      sh label: 'Terragrunt Plan',
+         script: 'terragrunt plan --terragrunt-non-interactive -out=plan.zip -input=false'  // # -var-file=base.tfvars -var-file="$ENV.tfvars"
     }
   }
 }
