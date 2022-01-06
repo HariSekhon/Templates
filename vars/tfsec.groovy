589ca297ac3b6299ff8269a4d45f629b15c25776
Hari Sekhon <harisekhon@gmail.com>
2022-01-06 17:28:25 +0000

    updated tfsec.groovy

--- vars/tfsec.groovy
+++ vars/tfsec.groovy
@@ -22,6 +22,7 @@ def call(timeoutMinutes=10){
         // aquasec/tfsec image is based on Alpine, doesn't have bash
         //sh '''#!/usr/bin/env bash -euxo pipefail
         //sh '''#!/bin/sh -eux
+        // use --no-color if not using ansicolor plugin
         sh 'tfsec --update'
         sh 'tfsec --version'
         sh' tfsec --run-statistics'  // nice summary table
