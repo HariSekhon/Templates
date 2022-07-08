9bb972954e5aed89896dbde6d481b1f3c03c6f88
Hari Sekhon <harisekhon@gmail.com>
2022-07-08 15:38:37 +0100

    updated k6.js

--- k6.js
+++ k6.js
@@ -15,6 +15,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// instead of setting on k6 CLI --vus 10 --duration 30s
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+
 export default function () {
   http.get('https://test.k6.io');
   sleep(1);
