88666039e487922df0971738823daa1cc1a727ea
Hari Sekhon <harisekhon@gmail.com>
2023-04-15 02:02:00 +0100

    updated diagram.py

--- diagram.py
+++ diagram.py
@@ -57,15 +57,63 @@ __version__ = '0.1'
 #   https://diagrams.mingrammer.com/docs/nodes/programming
 
 # pylint: disable=E0401
+
 from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+
+# aws resources
+from diagrams.aws.compute import ECS, Lambda
+from diagrams.aws.database import RDS, ElastiCache
+from diagrams.aws.network import ELB, Route53, VPC
+
+# azure resources
+from diagrams.azure.compute import FunctionApps
+from diagrams.azure.storage import BlobStorage
+
+# gcp resources
+from diagrams.gcp.compute import AppEngine, GKE
+from diagrams.gcp.ml import AutoML
+
+# k8s resources
+from diagrams.k8s.compute import Pod, StatefulSet
+from diagrams.k8s.network import Service
+from diagrams.k8s.storage import PV, PVC, StorageClass
 
 # diagram name results in 'web_service.png' as the output name
 # pylint: disable=W0106
 with Diagram("Web Service",
-             show=True,  # set to False to not auto-open the generated image file
+             show=True,        # set to False to not auto-open the generated image file
+             direction=LR,     # left-to-right, other options: TB, BT, LR, RL
              #outformat="jpg"  # default: png
+             #outformat=["jpg", "png", "dot"]  # or create all 3 format output files
+             #filename="my_diagram"  # override the default filename, without the extension
+             #
+             # GraphViz dot attributes are supported graph_attr, node_attr and edge_attr - create a dictionary{} of settings containing these attributes:
+             #
+             #  https://www.graphviz.org/doc/info/attrs.html
+             #
+             #graph_attr=graph_attr_settings_dict
+             #node_attr=node_attr_settings_dict
+             #edge_attr=edge_attr_settings_dict
              ):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+
+    # >>  right arrow
+    # <<  left arrow
+    # -   line with no arrow
+
+    # XXX: the order of rendered diagrams is the reverse of the declaration order
+
+    # appears at bottom
+    ELB("lb") >> EC2("web") >> RDS("userdb") >> S3("store")
+
+    ELB("lb") >> EC2("web") >> RDS("userdb") << EC2("stat")
+
+    # appears at top
+    # parens to protect against unexpected precedence results combining << >> with -
+    (ELB("lb") >> EC2("web")) - EC2("web") >> RDS("userdb")
+
+
+# Can render directly inside a Jupyter notebook like this:
+#
+# with Diagram("Simple Diagram") as diag:
+#     EC2("web")
+# diag
