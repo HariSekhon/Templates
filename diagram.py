911641371b824d061b513038059f30c20775781c
Hari Sekhon <harisekhon@gmail.com>
2023-04-27 20:04:46 +0100

    updated diagram.py

--- diagram.py
+++ diagram.py
@@ -35,44 +35,7 @@ __version__ = '0.1'
 # ============================================================================ #
 # https://diagrams.mingrammer.com/docs/getting-started/examples
 
-# Diagram reference objects:
-#
-#   # 'onprem' includes Cloud SaaS CI/CD and major Open Source products including:
-#   #
-#   # - VCS - Git, GitHub, GitLab, Subversion (Svn)
-#   # - CI/CD - Jenkins, ArgoCD, GitHub Actions, GitLab CI, CircleCI, Concourse, TeamCity, Tekton, Spinnaker
-#   # - Databases - MySQL, PostgreSQL, Cassandra, HBase, MongoDB, Oracle, Couchbase, Neo4J, InfluxDB
-#   # - Docker, Redis, Terraform, Vault, Ansible, Puppet, AWX, Atlantis
-#   # - Apache httpd, Nginx, Kong, Traefik, HAProxy, Consul, Etcd
-#   # - Analytics - Spark, Databricks, Kafka, Dbt, Flink, Hadoop, Hive, Presto, ZooKeeper, Storm, Airflow, Tableau
-#   # - Queues - Kafka, RabbitMQ, ActiveMQ, Celery
-#   # - Monitoring - Prometheus, Grafana, Datadog, Thanos, Splunk, Nagios
-#   # - K8s ecosystem - Prometheus, ArgoCD, FluxCD, Fluentd, Etcd, Cert Manager, Lets Encrypt
-#
-#   https://diagrams.mingrammer.com/docs/nodes/onprem
-#
-#   https://diagrams.mingrammer.com/docs/nodes/aws
-#
-#   https://diagrams.mingrammer.com/docs/nodes/azure
-#
-#   https://diagrams.mingrammer.com/docs/nodes/gcp
-#
-#   https://diagrams.mingrammer.com/docs/nodes/k8s
-#
-#   https://diagrams.mingrammer.com/docs/nodes/openstack
-#
-#   https://diagrams.mingrammer.com/docs/nodes/elastic
-#
-#   https://diagrams.mingrammer.com/docs/nodes/digitalocean
-#
-#   https://diagrams.mingrammer.com/docs/nodes/saas  # Snowflake, Newrelic, CDNs, Akamai, Cloudflare, Fastly,
-#                                                      Slack, Teams, Auth0, Okta, DataDog, Facebook, Twitter
-#
-#   https://diagrams.mingrammer.com/docs/nodes/generic  # Datacenter, Operating Systems, Virtualization, Mobile Devices
-#
-#   https://diagrams.mingrammer.com/docs/nodes/programming
-
-# pylint: disable=E0401,W0611
+# pylint: disable=E0401,W0611,C0301,W0404
 
 # ============================================================================ #
 
@@ -80,17 +43,65 @@ import os
 from diagrams import Diagram, Cluster, Edge
 
 # ============================================================================ #
+# On-premise / Open Source resources:
+#
+#   https://diagrams.mingrammer.com/docs/nodes/onprem
+#
+#   'onprem' includes Cloud SaaS CI/CD and major Open Source products including:
+#
+#   - CI/CD - Jenkins, ArgoCD, GitHub Actions, GitLab CI, CircleCI, Concourse, TeamCity, Tekton, Spinnaker
+#   - Databases - MySQL, PostgreSQL, Cassandra, HBase, MongoDB, Oracle, Couchbase, Neo4J, InfluxDB
+#   - Docker, Redis, Terraform, Vault, Ansible, Puppet, AWX, Atlantis
+#   - Apache httpd, Nginx, Kong, Traefik, HAProxy, Consul, Etcd
+#   - Analytics - Spark, Databricks, Kafka, Dbt, Flink, Hadoop, Hive, Presto, ZooKeeper, Storm, Airflow, Tableau
+#   - Queues - Kafka, RabbitMQ, ActiveMQ, Celery
+#   - Monitoring - Prometheus, Grafana, Datadog, Thanos, Splunk, Nagios
+#   - K8s ecosystem - Prometheus, ArgoCD, FluxCD, Fluentd, Etcd, Cert Manager, Lets Encrypt
+
+from diagrams.onprem.aggregator import Fluentd
+from diagrams.onprem.analytics import Beam, Databricks, Dbt, Flink, Hadoop, Hive, Presto, Spark, Superset, Tableau
+from diagrams.onprem.cd import Spinnaker, Tekton, TektonCli
+from diagrams.onprem.certificates import CertManager, LetsEncrypt
+from diagrams.onprem.ci import Jenkins, GithubActions, GitlabCI, CircleCI, ConcourseCI, Teamcity
+from diagrams.onprem.client import Users
+from diagrams.onprem.compute import Nomad, Server
+from diagrams.onprem.container import Containerd, Docker, K3S
+from diagrams.onprem.database import Cassandra, Couchbase, CouchDB, Druid, HBase, InfluxDB, MariaDB, MongoDB, MSSQL, MySQL, Neo4J, Oracle, PostgreSQL
+from diagrams.onprem.dns import Coredns, Powerdns
+from diagrams.onprem.gitops import ArgoCD, Flagger, Flux
+from diagrams.onprem.iac import Ansible, Atlantis, Awx, Puppet, Terraform
+from diagrams.onprem.inmemory import Aerospike, Hazelcast, Memcached, Redis
+from diagrams.onprem.monitoring import Datadog, Dynatrace, Grafana, Nagios, Newrelic, Prometheus, PrometheusOperator, Sentry, Splunk, Thanos, Zabbix
+from diagrams.onprem.logging import FluentBit, Graylog, Loki, RSyslog, SyslogNg
+from diagrams.onprem.network import Ambassador, Apache, Bind9, Consul, Envoy, Etcd, Glassfish, Gunicorn, HAProxy, Internet, Istio, Jetty, Kong, Nginx, Tomcat, Traefik, Zookeeper
+from diagrams.onprem.queue import ActiveMQ, Celery, Kafka, Nats, RabbitMQ, ZeroMQ
+from diagrams.onprem.registry import Harbor, Jfrog
+from diagrams.onprem.search import Solr
+from diagrams.onprem.security import Bitwarden, Trivy, Vault
+from diagrams.onprem.storage import Ceph, Glusterfs, Portworx
+from diagrams.onprem.tracing import Jaeger
+from diagrams.onprem.vcs import Git, Github, Gitlab, Svn
+from diagrams.onprem.workflow import Airflow, KubeFlow, NiFi
+
+# ============================================================================ #
 # AWS resources:
 #
 #   https://diagrams.mingrammer.com/docs/nodes/aws
 #
 
-from diagrams.aws.compute import EC2, ECS, EKS, Lambda
-from diagrams.aws.database import RDS, Redshift, ElastiCache, Aurora
-from diagrams.aws.integration import SQS
-from diagrams.aws.network import ELB, Route53, CloudFront, VPC
+from diagrams.aws.analytics import Athena, Cloudsearch, ElasticsearchService, Glue, Kinesis  # , Redshift
+from diagrams.aws.compute import Batch, Compute, EC2, ECR, ECS, EKS, Lambda
+from diagrams.aws.database import Aurora, ElastiCache, ElasticacheForRedis, RDS, Redshift, RDSInstance, RDSMariadbInstance, RDSMysqlInstance, RDSPostgresqlInstance, RDSSqlServerInstance
+from diagrams.aws.devtools import Codebuild, Codecommit, Codedeploy, Codepipeline, Codestar
+from diagrams.aws.general import User, Users
+from diagrams.aws.integration import SNS, SQS, StepFunctions
+from diagrams.aws.iot import IotGreengrass
+from diagrams.aws.management import AutoScaling, Cloudtrail, Cloudwatch, Config, ControlTower, Organizations, SSM, TrustedAdvisor, WellArchitectedTool
+from diagrams.aws.migration import DatabaseMigrationService, Datasync, DatasyncAgent, ServerMigrationService, Snowball, SnowballEdge, TransferForSftp
+from diagrams.aws.ml import Sagemaker
+from diagrams.aws.network import APIGateway, ClientVpn, CloudFront, ELB, ALB, NLB, Route53, VPC
+from diagrams.aws.security import KMS, Macie, SecretsManager, WAF
 from diagrams.aws.storage import S3
-from diagrams.aws.general import Users
 
 # ============================================================================ #
 # Azure resources:
@@ -98,8 +109,20 @@ from diagrams.aws.general import Users
 #   https://diagrams.mingrammer.com/docs/nodes/azure
 #
 
-from diagrams.azure.compute import FunctionApps
-from diagrams.azure.storage import BlobStorage
+from diagrams.azure.analytics import Databricks
+from diagrams.azure.compute import AvailabilitySets, AKS, ContainerInstances, ACR, FunctionApps, VM
+from diagrams.azure.database import BlobStorage, DataFactory, DataLake, DatabaseForMariadbServers, DatabaseForMysqlServers, DatabaseForPostgresqlServers, SQLDatabases, SQLServers, SQL
+from diagrams.azure.devops import Artifacts, Devops, Pipelines, Repos, TestPlans
+from diagrams.azure.general import Reservations, Resource, Resourcegroups, Tags
+from diagrams.azure.identity import ActiveDirectory, ADDomainServices, AppRegistrations, EnterpriseApplications, Groups, Users
+from diagrams.azure.integration import APIManagement, AppConfiguration, DataCatalog, LogicApps, ServiceBus, SoftwareAsAService
+from diagrams.azure.iot import DeviceProvisioningServices, IotHubSecurity, IotHub, Maps, Sphere
+from diagrams.azure.migration import DataBox, DataBoxEdge, DatabaseMigrationServices
+from diagrams.azure.ml import BatchAI, BotServices
+from diagrams.azure.network import ApplicationGateway, Firewall, LoadBalancers, Subnets, VirtualNetworks, VirtualNetworkGateways, VirtualWans
+from diagrams.azure.security import ApplicationSecurityGroups, KeyVaults, SecurityCenter, Sentinel
+from diagrams.azure.storage import BlobStorage, GeneralStorage, NetappFiles, StorageAccounts, StorageExplorer, TableStorage
+from diagrams.azure.web import APIConnections, AppServices, Search
 
 # ============================================================================ #
 # GCP resources:
@@ -107,13 +130,17 @@ from diagrams.azure.storage import BlobStorage
 #   https://diagrams.mingrammer.com/docs/nodes/gcp
 #
 
-from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
-from diagrams.gcp.compute import AppEngine, GKE, Functions
-from diagrams.gcp.database import BigTable
+from diagrams.gcp.analytics import BigQuery, Composer, DataCatalog, Dataflow, Dataprep, Dataproc, PubSub
+from diagrams.gcp.api import APIGateway, Endpoints
+from diagrams.gcp.compute import AppEngine, GCE, GKE, GCF, Run
+from diagrams.gcp.database import BigTable, Datastore, Memorystore, Spanner, SQL
+from diagrams.gcp.devtools import Build, Code, GCR, Scheduler, SDK, SourceRepositories
 from diagrams.gcp.iot import IotCore
-from diagrams.gcp.ml import AutoML
-from diagrams.gcp.network import DNS, LoadBalancing
-from diagrams.gcp.storage import GCS
+from diagrams.gcp.ml import AutoML, AutomlVision, InferenceAPI, NaturalLanguageAPI, RecommendationsAI, SpeechToText, TextToSpeech, TranslationAPI, VisionAPI
+from diagrams.gcp.network import Armor, CDN, DNS, ExternalIpAddresses, FirewallRules, LoadBalancing, NAT, Network, Router, Routes, StandardNetworkTier, TrafficDirector, VPC, VPN
+from diagrams.gcp.operations import Monitoring
+from diagrams.gcp.security import Iam, IAP, KMS, ResourceManager, SecurityCommandCenter, SecurityScanner
+from diagrams.gcp.storage import GCS, Filestore, PersistentDisk
 
 # ============================================================================ #
 # K8s resources:
@@ -121,34 +148,31 @@ from diagrams.gcp.storage import GCS
 #   https://diagrams.mingrammer.com/docs/nodes/k8s
 #
 
-from diagrams.k8s.clusterconfig import HPA
-from diagrams.k8s.compute import Deployment, Pod, ReplicaSet, StatefulSet
-from diagrams.k8s.network import Ingress, Service
-from diagrams.k8s.storage import PV, PVC, StorageClass
+from diagrams.k8s.clusterconfig import HPA, LimitRange, Quota
+from diagrams.k8s.compute import Cronjob, Deployment, DaemonSet, Job, Pod, ReplicaSet, StatefulSet
+from diagrams.k8s.controlplane import API, CCM, ControllerManager, Kubelet, KubeProxy, Scheduler
+from diagrams.k8s.ecosystem import ExternalDns, Helm, Krew, Kustomize
+from diagrams.k8s.group import Namespace
+from diagrams.k8s.infra import ETCD, Master, Node
+from diagrams.k8s.network import Endpoint, Ingress, NetworkPolicy, Service
+from diagrams.k8s.others import CRD, PSP
+from diagrams.k8s.podconfig import ConfigMap, Secret
+from diagrams.k8s.rbac import ClusterRole, ClusterRoleBinding, Group, RoleBinding, Role, ServiceAccount, User
+from diagrams.k8s.storage import PV, PVC, StorageClass, Volume
 
 # ============================================================================ #
-# On-premise / Open Source resources:
+# OpenStack resources:
 #
-#   https://diagrams.mingrammer.com/docs/nodes/onprem
+#   https://diagrams.mingrammer.com/docs/nodes/openstack
 #
 
-from diagrams.onprem.aggregator import Fluentd
-from diagrams.onprem.analytics import Spark
-from diagrams.onprem.certificates import CertManager, LetsEncrypt
-from diagrams.onprem.ci import Jenkins, GithubActions, CircleCI, ConcourseCI, Teamcity
-from diagrams.onprem.client import Users
-from diagrams.onprem.compute import Server
-from diagrams.onprem.database import PostgreSQL
-from diagrams.onprem.gitops import ArgoCD
-from diagrams.onprem.inmemory import Redis
-from diagrams.onprem.monitoring import Grafana, Prometheus
-from diagrams.onprem.network import Nginx, Kong, Traefik
-from diagrams.onprem.queue import Kafka
-from diagrams.onprem.vcs import Git, Github
-
+from diagrams.openstack.compute import Nova
+from diagrams.openstack.deployment import Ansible, Chef, Helm
+from diagrams.openstack.sharedservices import Glance, Keystone
+from diagrams.openstack.storage import Cinder, Swift
 
 # ============================================================================ #
-# SaaS:
+# SaaS resources:
 #
 #   https://diagrams.mingrammer.com/docs/nodes/saas
 #
@@ -159,6 +183,32 @@ from diagrams.saas.analytics import Snowflake
 from diagrams.saas.cdn import Akamai, Cloudflare, Fastly
 from diagrams.saas.chat import Slack, Teams
 from diagrams.saas.identity import Auth0, Okta
+from diagrams.saas.social import Facebook, Twitter
+
+# ============================================================================ #
+# Elastic resources:
+#
+#   https://diagrams.mingrammer.com/docs/nodes/elastic
+#
+
+from diagrams.elastic.agent import Agent
+from diagrams.elastic.beats import APM, Auditbeat, Filebeat, Functionbeat, Heartbeat, Metricbeat, Packetbeat
+from diagrams.elastic.elasticsearch import Alerting, Beats, ElasticSearch, Kibana, Logstash, MachineLearning, Maps, Monitoring, SQL, Stack
+from diagrams.elastic.enterprisesearch import AppSearch, Crawler, EnterpriseSearch
+from diagrams.elastic.observability import APM, Logs, Metrics, Observability, Uptime
+from diagrams.elastic.orchestration import ECE, ECK
+from diagrams.elastic.saas import Cloud, Elastic
+from diagrams.elastic.security import Security, SIEM
+
+# ============================================================================ #
+# DigitalOcean resources:
+#
+#   https://diagrams.mingrammer.com/docs/nodes/digitalocean
+
+from diagrams.digitalocean.compute import Containers, Docker, Droplet, K8SCluster, K8SNodePool, K8SNode
+from diagrams.digitalocean.database import DbaasPrimary, DbaasReadOnly, DbaasStandby
+from diagrams.digitalocean.network import Certificate, DomainRegistration, Domain, Firewall, FloatingIp, InternetGateway, LoadBalancer, ManagedVpn, Vpc
+from diagrams.digitalocean.storage import Folder, Space, VolumeSnapshot, Volume
 
 # ============================================================================ #
 # Generic - Datacentre, Operating Systems, Virtualization, Mobile Devices:
@@ -175,7 +225,6 @@ from diagrams.generic.os import IOS, Android, Raspbian, Windows
 from diagrams.generic.virtualization import Vmware, Virtualbox, XEN
 
 # ============================================================================ #
-#
 # Programming - flowcharts, programming languages and frameworks
 #
 #   https://diagrams.mingrammer.com/docs/nodes/programming
@@ -187,7 +236,6 @@ from diagrams.programming.language import Bash, Go, Python, R, Ruby, PHP, JavaSc
 from diagrams.programming.language import C, Cpp, Csharp, Java, Kotlin, Scala
 
 # ============================================================================ #
-#
 # C4 - Software Architecture - boxes with Name, Technology and Description inside
 #
 #   https://diagrams.mingrammer.com/docs/nodes/c4
@@ -196,7 +244,6 @@ from diagrams.programming.language import C, Cpp, Csharp, Java, Kotlin, Scala
 from diagrams.c4 import Person, Container, Database, System, SystemBoundary, Relationship
 
 # ============================================================================ #
-#
 # Custom - for creating a custom object using a downloaded image
 #
 #   https://diagrams.mingrammer.com/docs/nodes/custom
