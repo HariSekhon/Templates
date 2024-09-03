7a99ea2bbc34ffceefa35cb5b00d53bb4da5b016
Hari Sekhon <harisekhon@gmail.com>
2024-09-04 00:09:13 +0200

    updated .mdl.rb

--- .mdl.rb
+++ .mdl.rb
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2024-08-22 01:58:12 +0200 (Thu, 22 Aug 2024)
 #
-#  https///github.com/HariSekhon/Diagrams-as-Code
+#  https///github.com/HariSekhon/Templates
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
