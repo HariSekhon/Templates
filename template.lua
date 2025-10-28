bf322af1f5c2670ab8b0ebec8eac164a0ebce700
Hari Sekhon <harisekhon@gmail.com>
2025-10-28 20:42:34 +0300

    added template.lua

---
+++ template.lua
@@ -0,0 +1,15 @@
+--
+--  Author: Hari Sekhon
+--  Date: [% DATE  # 2025-10-28 20:42:08 +0300 (Tue, 28 Oct 2025) %]
+--
+--  vim:ts=4:sts=4:sw=4:et
+--
+--  https///github.com/HariSekhon/Templates
+--
+--  License: see accompanying Hari Sekhon LICENSE file
+--
+--  If you're using my code you're welcome to connect with me on LinkedIn
+--  and optionally send me feedback to help steer this or other code I publish
+--
+--  https://www.linkedin.com/in/HariSekhon
+--
